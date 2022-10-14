d19dfd8562bd7ed10a8118e279bdfdba50ee4557
b0rke-mborina <mborina@net.hr>
2022-10-14 19:11:59 +0200

    Ispravak Zadatka 6

--- 01-zadaci/zadatak6.py
+++ 01-zadaci/zadatak6.py
@@ -1,11 +1,11 @@
 def listsOrDicts(param1, param2):
-	if type(param1)!=type(param2): raise Exception()
-	if not (isinstance(param1, list) or isinstance(param1, dict)): raise Exception()
+	assert type(param1) == type(param2), "Parameters are not of the same type."
+	assert isinstance(param1, list) or isinstance(param1, dict), "Parameters are not lists or dictionaries."
 	if isinstance(param1, list): return param1+param2
 	else: return {**param1, **param2}
 
 print(listsOrDicts([1, 2, 1, 2], [3, 2]))
 print(listsOrDicts({1: 2, 3: 2}, {5: 2, 4: 1}))
-print(listsOrDicts((1, 2, 1, 2), (3, 2)))
 print(listsOrDicts({1: 2, 3: 2}, [3, 2]))
 print(listsOrDicts((1, 2, 1, 2), [3, 2]))
+print(listsOrDicts((1, 2, 1, 2), (3, 2)))

04200cd9526c2d23a8d258c8e7c61a676c321688
b0rke-mborina <mborina@net.hr>
2022-10-14 19:04:22 +0200

    Ispravak Zadatka 3

--- 01-zadaci/zadatak3.py
+++ 01-zadaci/zadatak3.py
@@ -1,7 +1,7 @@
 def articles(listOfDicts):
-	if not isinstance(listOfDicts, list): raise Exception()
-	if not all(isinstance(item, dict) for item in listOfDicts): raise Exception()
-	if not all(True if tuple(item)==("cijena", "naziv", "kolicina") else False for item in listOfDicts): raise Exception()
+	assert isinstance(listOfDicts, list), "Parameter is not a list."
+	assert all(isinstance(item, dict) for item in listOfDicts), "Not all list (parameter) elements are dictionaries."
+	assert all(True if tuple(item)==("cijena", "naziv", "kolicina") else False for item in listOfDicts), "Not all dictionaries have keys ('cijena', 'naziv', 'kolicina')"
 	artikli, cijena = [], 0
 	for artiklDict in listOfDicts:
 		artikli.append(artiklDict["naziv"])
@@ -10,5 +10,7 @@ def articles(listOfDicts):
 
 print(articles([{"cijena": 8, "naziv": "Kruh", "kolicina": 1}, {"cijena": 13, "naziv": "Sok", "kolicina": 2}, {"cijena": 7, "naziv": "Upaljac", "kolicina": 1}]))
 print(articles([{"cijena": 8, "naziv": "Kruh", "kolicina": 3}, {"cijena": 8, "naziv": "Sok", "kolicina": 10}]))
+print(articles(({"cijena": 8, "naziv": "Kruh", "kolicina": 3}, {"cijena": 8, "naziv": "Sok", "kolicina": 10})))
+print(articles([{"cijena": 8, "naziv": "Kruh", "kolicina": 3}, {"cijena", "naziv", "kolicina"}]))
 print(articles([{"kolicina": 8, "naziv": "Kruh", "cijena": 3}, {"cijena": 3, "naziv": "Sok", "kolicina": 10}]))
 print(articles([{"Cijena": 8, "naziv": "Kruh", "kolicina": 3}, {"cijena": 8, "naziv": "Sok", "kolicina": 10}]))

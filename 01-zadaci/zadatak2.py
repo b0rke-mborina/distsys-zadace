15dc3c302143fece5d2eb1dee0f16cd2010c38b1
b0rke-mborina <mborina@net.hr>
2022-10-14 18:57:45 +0200

    Ispravak Zadatka 2

--- 01-zadaci/zadatak2.py
+++ 01-zadaci/zadatak2.py
@@ -1,6 +1,7 @@
 def listAndDict(listArg, dictArg):
-	if not isinstance(listArg, list) or not isinstance(dictArg, dict): raise Exception()
-	if not (len(listArg)==len(dictArg) and all(isinstance(item, int) for item in listArg)): raise Exception()
+	assert isinstance(listArg, list) and isinstance(dictArg, dict), "Parameters are not a list and a dictionary respectively."
+	assert len(listArg)==len(dictArg), "Parameters are not of equal length."
+	assert all(isinstance(item, int) for item in listArg), "Not all list elements are integers."
 	return {dictItem:listItem if 5<=listItem<=10 else -1 for listItem,dictItem in zip(listArg, dictArg)}
 
 print(listAndDict([8, 7, 1], {1: 2, 2: 1, 3: 2}))
